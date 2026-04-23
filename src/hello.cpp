185540d2c7979fa59ff503c5a1a1f14cab754995
tvbi30 <tobias.mathena@email.uni-freiburg.de>
2026-04-23 14:39:20 +0200

    dateien entfernt

--- src/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include "hello.h"
-#include <iostream>
-
-void hello_world() {
-    std::cout << "Hello world!" << std::endl;
-}
--- src/hello.h
+++
@@ -1,8 +0,0 @@
-#ifndef VERLET_H
-#define VERLET_H
-
-
-void hello_world();
-
-
-#endif // __VERLET_H
